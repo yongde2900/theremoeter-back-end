fa616350264d8f7ab520263d3379653f5e8b8f1b
ej0331 <k0970133227@gmail.com>
2023-02-09 15:47:32 +0800

    新增定時紀錄歷史資料功能

---
+++ firebaseHandler.js
@@ -0,0 +1,36 @@
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+const db = getFirestore()
+
+const dataCollection = db.collection('main')
+const historyCollection = db.collection('history')
+
+const saveData = async (data) => {
+    const dateTime = Date.now();
+    const timestamp = Math.floor(dateTime / 1000);
+    const doc = dataCollection.doc()
+    return await doc.set(data)
+}
+
+const saveHistory = async (dataList, type) => {
+
+    let doc = historyCollection.doc()
+    let temperatures = 0
+    let humidities = 0
+    let earliestTimestamp = dataList[0].timestamp
+    let latestTimestamp = dataList[dataList.length - 1].timestamp
+    for (var i = 0; i < dataList.length; i++) {
+        temperatures += dataList[i].temperature
+        humidities += dataList[i].humidity
+    }
+    data = {
+        'type': type,
+        'temperature_avg' : temperatures / dataList.length,
+        'humidity_avg' : humidities /  dataList.length,
+        'earliest_timestamp' : earliestTimestamp,
+        'latest_timestamp' : latestTimestamp,
+    }
+    
+    return await doc.set(data)
+}
+
+module.exports = {saveData, saveHistory}
