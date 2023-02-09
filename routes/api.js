40331fb9c6866af71c9d36b956ac8d14b26c2639
yongde2900 <com414141@gmail.com>
2023-02-09 17:45:31 +0800

    feat: creat GET /api/dats/:type api

--- routes/api.js
+++ routes/api.js
@@ -1,36 +1,58 @@
 const express = require('express')
 const router = express.Router()
-const mqtt = require('mqtt')
 const { getFirestore } = require('firebase-admin/firestore');
 const db = getFirestore()
+const main = db.collection('main')
+const history = db.collection('history')
 
-const option = {
-  username: 'user',
-  password: '123456'
-}
+
 
 // get the real time data from broker
-router.get('/data', async(req, res) => {
-  const main = db.collection('main').orderBy('timestamp','desc').limit(1)
-  const snapshot = await main.get()
-  let datas = []
-  snapshot.forEach(doc => {
-    datas.push(doc.data())
-  })
+router.get('/data', async (req, res) => {
+  const datas = await getDataFromMain(1)
   res.json(datas[0])
 })
 
-//get the  history data from database
+//get the  history data from main
 router.get('/datas', async (req, res) => {
   let limit = parseInt(req.query.limit)
   limit = limit ? limit : 10
-  const main = db.collection('main').orderBy('timestamp','desc').limit(limit)
-  const snapshot = await main.get()
-  let datas = []
-  snapshot.forEach(doc => {
-    datas.push(doc.data())
-  })
+  const datas = await getDataFromMain(limit)
+  res.json(datas)
+})
+
+//get history data with type
+router.get('/datas/:type', async (req, res) => {
+  const type = req.params.type
+  const datas = await getDataFromHistory(type)
   res.json(datas)
 })
 
 module.exports = router
+
+async function getDataFromHistory(type) {
+  const amount = {
+    quarter: 4,
+    hour: 24,
+    day: 7
+  }
+
+  const snapshot = await history.where('type', '=', type).limit(amount[type]).orderBy('earliest_timestamp', 'desc').get()
+  let datas = []
+  snapshot.forEach(doc => {
+    const data = doc.data()
+    data.humidity_avg = Math.floor(data.humidity_avg, -2)
+    data.temperature_avg = Math.floor(data.temperature_avg, -2)
+    datas.push(data)
+  })
+  return datas
+}
+
+async function getDataFromMain(number) {
+  const snapshot = await main.orderBy('timestamp', 'desc').limit(number).get()
+  let datas = []
+  snapshot.forEach(doc => {
+    datas.push(doc.data())
+  })
+  return datas
+}
