145430e085fd65e9d70e54d827e3918abc1a62a2
yongde2900 <com414141@gmail.com>
2023-02-18 16:37:36 +0800

    chore: ignore firebase admin

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const serviceAccount = require('../trunk-raspberry-pi-tah-firebase-adminsdk-pxjji-d8f129d255.json');
+const serviceAccount = require('./trunk-raspberry-pi-tah-firebase-adminsdk-pxjji-d8f129d255.json');
 
 module.exports = () => {
     initializeApp({ credential: cert(serviceAccount) })
